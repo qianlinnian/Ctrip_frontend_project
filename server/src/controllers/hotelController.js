c68d785ca13eaf7929a7a90003157900a1a65cbd
owlish <14942248+owlish@user.noreply.gitee.com>
2026-02-26 15:10:13 +0800

    fix:修复后端无法正确处理错误导致请求挂起的问题

--- server/src/controllers/hotelController.js
+++ server/src/controllers/hotelController.js
@@ -12,8 +12,8 @@ exports.updateHotelAndRoom = async (req, res) => {
         
 
         //更新酒店字段
-        const updatedHotel = await updateHotel(req.body)
-        if(!updateHotel) {
+        const updatedHotelResult = await updateHotel(req.body)
+        if(!updatedHotelResult) {
             return res.status(500).json({message: 'Failed to update hotel'})
         }
         console.log('Hotel updated successfully')
@@ -40,7 +40,7 @@ exports.updateHotelAndRoom = async (req, res) => {
         if(!updatePriceResult) {
             return res.status(500).json({message: 'Failed to update price'})
         }
-        res.status(200).json({message: 'Hotel&Room updated successfully', hotel: updatedHotel})
+        res.status(200).json({message: 'Hotel&Room updated successfully', hotel: updatedHotelResult})
     } catch (error) {
         console.error('Update failed:', error.message)
         res.status(500).json({ message: 'Update failed', error: error.message })
@@ -53,93 +53,133 @@ exports.updateHotelAndRoom = async (req, res) => {
 exports.addHotel = async(req, res) => {
 
     //查询酒店名称是否已存在
-    const existHotel = await findHotelByName(req.body.hotel_name)
-    if(existHotel) {
-        return res.status(400).json({messaage: 'Hotel name already exist'})
-    }
-
-    let hotelData = req.body
-    hotelData = {...hotelData, audit_status: 1, publish_status: 0}
-    const tags = req.body.tags
-    
-    const addHotelResult = await insertHotel(hotelData)
-    if(!addHotelResult) {
-        return res.status(500).json({message: 'Failed to add hotel'})
-    }
-    if(tags) {
-    for(const tag of tags) {
-        const insertTagResult = await insertTag({hotel_id: addHotelResult.insertId, tag_id: tag})
-        if(!insertTagResult) {
-            return res.status(500).json({message: 'Failed to insert tag'})
+    try {
+        const existHotel = await findHotelByName(req.body.hotel_name)
+        if(existHotel) {
+            return res.status(400).json({message: 'Hotel name already exist'})
         }
+
+        let hotelData = req.body
+        hotelData = {...hotelData, audit_status: 1, publish_status: 0}
+        const tags = req.body.tags
+        
+        const addHotelResult = await insertHotel(hotelData)
+        if(!addHotelResult) {
+            return res.status(500).json({message: 'Failed to add hotel'})
+        }
+        if(tags) {
+            for(const tag of tags) {
+                const insertTagResult = await insertTag({hotel_id: addHotelResult.insertId, tag_id: tag})
+                if(!insertTagResult) {
+                    return res.status(500).json({message: 'Failed to insert tag'})
+                }
+            }
+            res.status(200).json({message: 'Hotel added successfully', hotel: addHotelResult})
+        }else{
+            res.status(200).json({message: 'Hotel added successfully', hotel: addHotelResult})
+        }
+    } catch (error) {
+        console.error('Error adding hotel:', error.message)
+        return res.status(500).json({message: 'Internal Server Error'})
     }
-    }
-    res.status(200).json({message: 'Hotel added successfully', hotel: addHotelResult})
+   
+    
 }
 
 
 //获取商户名下酒店列表
 exports.getMyHotels = async (req, res) => {
-    const {merchant_id} = req.body
-
-
-    const hotels = await findMyHotels(merchant_id)
-    if(!hotels) {
-        return res.status(200).json({message: 'Hotels found successfully', hotels})
-    } else {
-        return res.status(200).json({message: 'Hotels found successfully', hotels})
+    try{
+        const {merchant_id} = req.body
+        const hotels = await findMyHotels(merchant_id)
+        if(!hotels) {
+            return res.status(400).json({message: 'Hotels not found'})
+        } else {
+            return res.status(200).json({message: 'Hotels found successfully', hotels})
+        }
+    } catch (error) {
+        console.error('Error fetching hotels:', error.message)
+        return res.status(500).json({message: 'Internal Server Error'})
     }
+    
 }
 
  
 //根据id获取酒店信息&&房间信息
 exports.getHotelInfo = async (req, res) => {
-    const {hotel_id} = req.params
-    console.log('option in getHotelInfo, hotel_id=',hotel_id)
-    const hotelResult = await findHotelById(hotel_id)
-    if(!hotelResult) {
-        return res.status(200).json({message: 'Hotel found successfully', hotel: hotelResult})
-    } else {
-        return res.status(200).json({message: 'Hotel found successfully', hotel: hotelResult})
+    try{
+        const {hotel_id} = req.params
+        console.log('option in getHotelInfo, hotel_id=',hotel_id)
+        const hotelResult = await findHotelById(hotel_id)
+        if(!hotelResult) {
+            return res.status(404).json({message: 'Hotel not found'})
+        } else {
+            return res.status(200).json({message: 'Hotel found successfully', hotel: hotelResult})
+        }
+    } catch (error) {
+        console.error('Error fetching hotel:', error.message)
+        return res.status(500).json({message: 'Internal Server Error'})
     }
 }
 
 exports.getRoomInfo = async (req, res) => {
-    const {hotel_id} = req.params
-    const roomResult = await findRoomByHotelId(hotel_id)
-    console.log('RoomResult:', roomResult)
-    if(!roomResult) {
-        return res.status(200).json({message: 'Room found successfully', room: roomResult})
-    } else {
-        return res.status(200).json({message: 'Room found successfully', room: roomResult})
+    try{
+
+        const {hotel_id} = req.params
+        const roomResult = await findRoomByHotelId(hotel_id)
+        console.log('RoomResult:', roomResult)
+        if(!roomResult) {
+            return res.status(404).json({message: 'Room not found'})
+        } else {
+            return res.status(200).json({message: 'Room found successfully', room: roomResult})
+        }
+    } catch (error) {
+        console.error('Error fetching room:', error.message)
+        return res.status(500).json({message: 'Internal Server Error'})
     }
 }
 
 
 exports.getRoomType = async( req, res) => {
-    const roomTypeOptions = await getRoomType()
-    const options = roomTypeOptions.map(option => ({ 
-        label: option.type_name,
-        value: option.type_id }))
-    res.status(200).json({message: 'Room type found successfully', options})
+    try {
+        const roomTypeOptions = await getRoomType()
+        const options = roomTypeOptions.map(option => ({ 
+            label: option.type_name,
+            value: option.type_id }))
+        res.status(200).json({message: 'Room type found successfully', options})
+    } catch (error) {
+        console.error('Error fetching room type:', error.message)
+        return res.status(500).json({message: 'Internal Server Error'})
+    }
 }
 
 
 exports.getTags = async(req, res) => {
-    const tags = await getTags()
-    const options = tags.map(option => ({ 
-        label: option.tag_name,
-        value: option.tag_id }))
-    res.status(200).json({message: 'Tags found successfully', options})
+    try {
+        const tags = await getTags()
+        const options = tags.map(option => ({ 
+            label: option.tag_name,
+            value: option.tag_id }))
+        res.status(200).json({message: 'Tags found successfully', options})
+    } catch (error) {
+        console.error('Error fetching tags:', error.message)
+        return res.status(500).json({message: 'Internal Server Error'})
+    }
+    
 }
 
 
 exports.getHotelTags = async(req, res) => {
-    const hotel_id = req.params.hotel_id
-    const tags = await getTagsByHotelId(hotel_id)
-    const options = tags.map(option => ({ 
-        label: option.tag_name,
-        value: option.tag_id }))
-    res.status(200).json({message: 'Hotel tags found successfully', options})
+    try {
+        const hotel_id = req.params.hotel_id
+        const tags = await getTagsByHotelId(hotel_id)
+        const options = tags.map(option => ({ 
+            label: option.tag_name,
+            value: option.tag_id }))
+        res.status(200).json({message: 'Hotel tags found successfully', options})
+    } catch (error) {
+        console.error('Error fetching hotel tags:', error.message)
+        return res.status(500).json({message: 'Internal Server Error'})
+    }
 }
 
